518a3077744d80b4c2200062890d6a7d4a717d4f
Simon Meyer <simonmeyerrr@gmail.com>
2021-01-30 03:42:43 +0100

    merge conflict

--- public/js/game.js
+++ public/js/game.js
@@ -1,1 +1,19 @@
-console.log("game");
+const getGameId = () => {
+    const splitedPath = window.location.pathname.split("/");
+    return splitedPath[splitedPath.length - 1];
+};
+
+const isSecure = () => {
+    return window.location.protocol === "https:";
+};
+
+const setConnectionPage = () => {
+    const element = document.getElementById("connection");
+    element.innerHTML = '<>'
+};
+
+export {
+    getGameId,
+    isSecure,
+    setConnectionPage
+};
